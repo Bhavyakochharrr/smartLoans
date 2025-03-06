4e761a1ce1de24cb08901b2005b05ec4ca05cf9e
Anantbhavya Kochhar <anantbhavya.k@brillio.com>
2025-03-06 17:23:12 +0530

    new main

--- smartLoans_frontend/server/main.js
+++ smartLoans_frontend/server/main.js
@@ -1,13 +1,18 @@
-import {httpx,expressx} from 'ca-webutils';
+import { httpx } from 'ca-webutils';
 import express from 'express';
 import path from 'path';
-const app=express();
-app.use(express.static('./dist'));
- 
-app.get('*',(request,response)=>{
-    response.send(path.join('./dist','index.html'));
- 
-})
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
 httpx.runApp({
-    requestHandler:app
-})
+    requestHandler: app
+});
